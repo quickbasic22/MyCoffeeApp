4b27e665bb69ec8482226d3ade2c6482fbb0c19e
quickbasic22 <quickbasic44@outlook.com>
2021-11-15 01:09:15 -0500

    Add project files.

---
+++ MyCoffeeApp/MyCoffeeApp/Views/CoffeeEquipment.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCoffeeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoffeeEquipment : ContentPage
+    {
+        public CoffeeEquipment()
+        {
+            InitializeComponent();
+            IncreaseCount = new Command(OnIncrease);
+            BindingContext = this;
+        }
+
+        public ICommand IncreaseCount { get; }
+
+        int count = 0;
+        string countDisplay = "Click Me!";
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay)
+                    return;
+                countDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"You clicked {count} time(s)";
+        }
+
+      
+    }
+}
